5573f3ce708dec4b103afedceb498cb8e1c5db32
Dexin <tanx3406@gmail.com>
2023-07-03 10:42:21 +0800

    Side bar nav

---
+++ ninja-code/src/components/HeroSection/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <HeroContainer>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4'></>
+    </HeroContainer>
+  )
+}
+
+export default index;
